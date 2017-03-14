02f795f57eaad744fc34151d2c4ca0e11c82c213
Jessica H <Jessica H>
2017-03-14 10:24:04 -0400

    Revised boiler-plate

--- src/app.js
+++ src/app.js
@@ -1,23 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Search from './searchbar.js'
-
-class Submitbutton extends React.Component {
-	render() {
-		return (
-			<button className="search">Search</button>
-		)
-	}
-}
-
 class App extends React.Component {
 	render() {
 		return (
 			<header>
 				<h1>Hi</h1>  
 				<p>This is a paragraph</p>
-				<Search /><Submitbutton />
 			</header>
 		)
 	}
