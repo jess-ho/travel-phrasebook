1199c7b88cf1db7f2571d84354906ec06b1f6b88
Jessica H <Jessica H>
2017-03-15 10:15:40 -0400

    test for api

--- src/app.js
+++ src/app.js
@@ -5,14 +5,43 @@ import { ajax } from 'jquery'
 import Header from './components/header.js'
 import Footer from './components/footer.js'
 
+const translate = require('google-translate-api');
+
 class App extends React.Component {
+	constructor() {
+		super()
+		this.state = {
+			phrase: '',
+			phraseLog: []
+		}
+		this.handleSubmit = this.handleSubmit.bind(this)
+	}
+	componentDidMount() {
+		translate('Ik spreek Engels', {to: 'en'}).then(res => {
+		    console.log(res.text);
+		    //=> I speak English
+		    console.log(res.from.language.iso);
+		    //=> nl
+		}).catch(err => {
+		    console.error(err);
+		});
+	}
+	handleChange(e) {
+		this.setState({
+			[e.target.name]: [e.target.value]
+		})
+	}
+	handleSubmit(e) {
+		e.preventDefault()
+
+	}
 	render() {
 		return (
 			<main>
 				<Header />
 
-				<form>
-					<input />
+				<form onSubmit={this.handleSubmit}>
+					<input onChange={this.handleChange}/>
 					<input />
 
 					<div></div>
